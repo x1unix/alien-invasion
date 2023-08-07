2a0e1fc9dffb6fae80d3bb447fc72315120ffff0
x1unix <denis0051@gmail.com>
2023-08-07 06:48:02 +0200

    feat: add game tests

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/x1unix/alien-invasion
 go 1.20
 
 require (
+	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- internal/invasion/game_test.go
+++ internal/invasion/game_test.go
@@ -1,39 +1,87 @@
 package invasion
 
 import (
+	"path/filepath"
+	"sort"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
+	"github.com/x1unix/alien-invasion/internal/invasion/mock"
 	"github.com/x1unix/alien-invasion/internal/mapfile"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
 )
 
-func TestGame_removeCity(t *testing.T) {
-	m, err := mapfile.ReadFile("../../map.txt")
-	require.NoError(t, err)
+func TestGame_Tick(t *testing.T) {
+	cases := map[string]struct {
+		aliensCount int
+		tickLimit   uint
+		mapFile     string
 
-	game := NewGame(1, 10, m.Cities)
-	//game.Tick()
-
-	c, ok := game.cities["Whiterun"]
-	game.aliens[0].CurrentCity = game.cities["Windhelm"]
-	require.True(t, ok)
-	game.removeCity(c)
-	t.Log()
-}
-
-func TestGame(t *testing.T) {
-	m, err := mapfile.ReadFile("../../cites.txt")
-	require.NoError(t, err)
-
-	game := NewGame(10, 10000, m.Cities)
-	places := []string{
-		"Red_Bank", "Elizabeth", "Newark",
-		"Yonkers", "New_York", "Woodbridge",
-		"Stamford", "Port_Chester", "Rye", "New_Rochelle",
+		prepareRand    func(ctrl *mock.MockRand)
+		prepareGame    func(game *Game)
+		checkGameState func(t *testing.T, game *Game)
+	}{
+		"respects tick limit": {
+			tickLimit:   2,
+			aliensCount: 1,
+			mapFile:     "skyrim.txt",
+			checkGameState: func(t *testing.T, game *Game) {
+				require.LessOrEqual(t, uint(2), game.tickCount)
+			},
+		},
+		"should destroy city on intersect": {
+			aliensCount: 2,
+			tickLimit:   10000,
+			mapFile:     "collapse.txt",
+			prepareGame: func(game *Game) {
+				game.aliens = []*Alien{
+					NewAlien(0, game.cities["left"]),
+					NewAlien(1, game.cities["right"]),
+				}
+			},
+			checkGameState: func(t *testing.T, game *Game) {
+				wantCities := []string{"left", "right"}
+				got := game.cities.Names()
+				sort.Strings(got)
+				require.Equal(t, wantCities, got)
+				require.Empty(t, game.aliens)
+			},
+		},
 	}
 
-	for i, place := range places {
-		game.aliens[i].CurrentCity = m.Cities[place]
-	}
+	for n, c := range cases {
+		t.Run(n, func(t *testing.T) {
+			f, err := mapfile.ReadFile(filepath.Join("testdata", c.mapFile))
+			require.NoError(t, err, "failed to read test map file")
+			game := NewGame(c.aliensCount, c.tickLimit, f.Cities)
 
+			zap.ReplaceGlobals(zaptest.NewLogger(t, zaptest.Level(zapcore.DebugLevel)))
+			t.Cleanup(zap.ReplaceGlobals(zap.NewNop()))
+
+			if c.prepareRand != nil {
+				t.Cleanup(func() {
+					rnd = originalRand
+				})
+
+				ctrl := gomock.NewController(t)
+				r := mock.NewMockRand(ctrl)
+				c.prepareRand(r)
+				rnd = r
+			}
+
+			if c.prepareGame != nil {
+				c.prepareGame(game)
+			}
+
+			for game.Tick() {
+			}
+
+			if c.checkGameState != nil {
+				c.checkGameState(t, game)
+			}
+		})
+	}
 }
